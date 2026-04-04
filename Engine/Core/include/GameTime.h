4c6ad1758ae8dfb4e384b31e581db4a5ddd72c3d
James Mills <jcmills.developer@gmail.com>
2026-04-05 08:42:26 +1000

    Working on game time system

---
+++ Engine/Core/include/GameTime.h
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ * GameTime
+ * Created by James Mills on 5/04/2026.
+ *****************************************************************************/
+
+#pragma once
+
+#include <chrono>
+
+#include "catalyst_export.h"
+
+using std::chrono::steady_clock;
+
+namespace Catalyst
+{
+	class CATALYST_EXPORT GameTime
+	{
+	public:
+		friend class Application;
+
+	public:
+		static float DeltaTime();
+		static float AppTime();
+		static uint32 FPS();
+
+	private:
+		static steady_clock::time_point m_currTime;
+		static steady_clock::time_point m_prevTime;
+
+		static steady_clock::time_point m_initialTime;
+
+		static float m_deltaTime;
+		static uint32 m_frames;
+		static float m_fpsInterval;
+		static uint32 m_fps;
+
+	private:
+		static void Init();
+		static void Tick();
+	};
+}
